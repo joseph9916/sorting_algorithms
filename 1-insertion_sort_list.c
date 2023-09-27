9e3bd7829c9ced2d5ea73e97a8b1c39761676b92
joseph9916 <ebunopeajayi@gmail.com>
2023-09-27 19:55:15 +0000

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * swap_node - swaps the nodes
+ * @prevnode: the previous node
+ * @curnode: the current node
+ */
+
+void swap_node(listint_t *prevnode, listint_t *curnode)
+{
+	listint_t *tempnode;
+
+	tempnode = prevnode->prev;
+	prevnode->prev = curnode;
+	curnode->prev = tempnode;
+	if (tempnode)
+		tempnode->next = curnode;
+	tempnode = curnode->next;
+	curnode->next = prevnode;
+	prevnode->next = tempnode;
+	if (tempnode)
+		tempnode->prev = prevnode;
+}
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: linked list to sort
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curnode, *present_node = (*list)->next, *prevnode, *tempnode;
+
+	if (!present_node)
+		return;
+	if (!present_node->next)
+	{
+		curnode = present_node;
+		prevnode = curnode->prev;
+		if (curnode->n < prevnode->n)
+		{
+			prevnode->next = NULL;
+			curnode->prev = NULL;
+			prevnode->prev = curnode;
+			curnode->next = prevnode;
+			*list = curnode;
+			print_list(*list);
+		}
+		return;
+	}
+	while (present_node)
+	{
+		curnode = present_node;
+		present_node = present_node->next;
+		prevnode = curnode->prev;
+		while (prevnode)
+		{
+			if (prevnode->n > curnode->n)
+			{
+				swap_node(prevnode, curnode);
+				tempnode = curnode;
+				curnode = prevnode;
+				prevnode = tempnode;
+				if (curnode == *list)
+					*list = prevnode;
+				print_list(*list);
+			}
+			curnode = curnode->prev;
+			prevnode = curnode->prev;
+		}
+	}
+}
